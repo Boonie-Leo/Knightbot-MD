279ecc4a365f053e6d54f3bc76fc168233eb7360
Boonie-Leo <codephantombunny@gmail.com>
2026-07-10 08:50:39 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Codey Bot',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "Codey Bot",
   botOwner: 'Codey Leo', // Your name
   ownerNumber: '254755784300', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
